f081971d58d7a07de180d7793fb374b468b5a00f
Ubuntu <brianpenghe@gmail.com>
2019-09-02 13:36:31 +0000

    now save the significant PC IDs

--- JackstrawSummary.R
+++ JackstrawSummary.R
@@ -1,10 +1,12 @@
 #This set of functions prints Jackstraw results of a Seurat object and reports which are significant
 #Note that the number of PCs needs to be dividable by the row number you specified
-JackstrawSummary <- function(Seurat, nrow=50){
+#It returns a list of significant PCs
+JackstrawSummary <- function(Seurat, nrow=50, cutoff = 0.05){
         npc <- dim(Seurat@reductions$pca@jackstraw@overall.p.values)[1]
 	JStable=updated_MouseLimb10X@reductions$pca@jackstraw@overall.p.values[1:nrow,]
         for (i in 1:floor(npc/nrow-1)){
 		JStable <- cbind(JStable,updated_MouseLimb10X@reductions$pca@jackstraw@overall.p.values[(nrow*i+1):(nrow*i+50),])
 	}
-	JStable
+	print(JStable)
+	return(Seurat@reductions$pca@jackstraw@overall.p.values[Seurat@reductions$pca@jackstraw@overall.p.values[,2]<cutoff,1])
 }

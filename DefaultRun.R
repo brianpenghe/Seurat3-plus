3138d2e6385460f4cd90395145ffb1b749197635
Ubuntu <brianpenghe@gmail.com>
2019-09-05 08:42:04 +0000

    added regression against quality

--- DefaultRun.R
+++ DefaultRun.R
@@ -1,9 +1,14 @@
 #This set of functions blindly processes the data without any optimizations
 #It returns an object
-DefaultRun <- function(Seurat,dim=1:50,nfeatures = 2000){
+DefaultRun <- function(Seurat,dim=1:50,nfeatures = 2000, regress = TRUE){
 	Lung <- NormalizeData(Seurat, assay='RNA')
 	Lung <- FindVariableFeatures(Lung, assay='RNA',nfeatures = nfeatures)
-	Lung <- ScaleData(object = Lung, assay = 'RNA')
+	if (regress == TRUE){
+		Lung[["percent.mt"]] <- PercentageFeatureSet(Lung, pattern = "^MT-")
+		Lung <- ScaleData(object = Lung, assay = 'RNA',vars.to.regress = c("percent.mt","nCount_RNA"))
+	} else {
+		Lung <- ScaleData(object = Lung, assay = 'RNA')
+	}
 	Lung <- RunPCA(object = Lung,assay = 'RNA',seed.use=1,npcs=50)
 	Lung <- FindNeighbors(object = Lung,assay='RNA',dims=dim)
 	Lung <- FindClusters(object = Lung, random.seed = 1,resolution=0.5)

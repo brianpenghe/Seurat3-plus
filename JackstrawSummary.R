2f685c9c35dbd1f256ee6a2c555b5d0e9643792f
Ubuntu <brianpenghe@gmail.com>
2019-10-19 14:11:09 +0000

    corrected seurat object's name

--- JackstrawSummary.R
+++ JackstrawSummary.R
@@ -3,9 +3,9 @@
 #It returns a list of significant PCs
 JackstrawSummary <- function(Seurat, nrow=50, cutoff = 0.05){
         npc <- dim(Seurat@reductions$pca@jackstraw@overall.p.values)[1]
-	JStable=updated_MouseLimb10X@reductions$pca@jackstraw@overall.p.values[1:nrow,]
+	JStable=Seurat@reductions$pca@jackstraw@overall.p.values[1:nrow,]
         for (i in 1:floor(npc/nrow-1)){
-		JStable <- cbind(JStable,updated_MouseLimb10X@reductions$pca@jackstraw@overall.p.values[(nrow*i+1):(nrow*i+50),])
+		JStable <- cbind(JStable,Seurat@reductions$pca@jackstraw@overall.p.values[(nrow*i+1):(nrow*i+50),])
 	}
 	print(JStable)
 	return(Seurat@reductions$pca@jackstraw@overall.p.values[Seurat@reductions$pca@jackstraw@overall.p.values[,2]<cutoff,1])

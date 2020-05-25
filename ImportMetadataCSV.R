2a82824c284d0517cef7991eb5faf736b7284007
Ubuntu <brianpenghe@gmail.com>
2020-05-25 17:34:04 +0000

    added a script to import Metadata from data.frame into Seurat object

---
+++ ImportMetadataCSV.R
@@ -0,0 +1,10 @@
+#This set of functions prints Jackstraw results of a Seurat object and reports which are significant
+#Note that the number of PCs needs to be dividable by the row number you specified
+#It returns a list of significant PCs
+ImportMetadataCSV <- function(Seurat,Meta){
+        ##Meta is a data.frame of metadata columns - one feature per column
+	for (item in colnames(Meta)){
+            Seurat <- AddMetaData(Seurat, Meta[,item], col.name = item)
+        }
+	return(Seurat)
+}

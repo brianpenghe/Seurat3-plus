61f9754404bbf5ebcf240bde97dcb742942db774
brianpenghe <brianpenghe@gmail.com>
2020-12-30 14:06:16 +0000

    now skip no-marker error of the pairwise marker function

--- PairwiseMarkers.R
+++ PairwiseMarkers.R
@@ -1,17 +1,19 @@
 #This set of functions blindly processes the data without any optimizations
 #It returns an object
 PairwiseMarkers <- function(C1,min.pct=0.25,min.diff.pct=0.2){
-	markers=NA
+        markers=NA
         for (i in levels(Idents(C1))){
             for (j in levels(Idents(C1))){
-                if (i==j){}
-                else{
-                    temp<-FindMarkers(object = C1, ident.1 = i, 
-                    ident.2 = j,min.pct=min.pct,min.diff.pct=min.diff.pct,
+                tryCatch({
+                    if (i==j){}
+                    else{
+                        temp<-FindMarkers(object = C1, ident.1 = i,
+                        ident.2 = j,min.pct=min.pct,min.diff.pct=min.diff.pct,
                        only.pos=TRUE)
-                    temp$compare <- paste(i,j, sep = " VS ")
-                    markers<-rbind(markers,temp)}
-    }
-}
-	return(markers)
-}
+                        temp$compare <- paste(i,j, sep = " VS ")
+                        markers<-rbind(markers,temp)}
+                },error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+                                            }
+                                      }
+        return(markers)
+                                                             }
